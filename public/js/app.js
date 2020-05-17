d90466a48e8f41424f822b17f1df23e6dfdf2b27
Utkarsh Mehta <utkarsh.mehta@renaissance.com>
2020-05-17 10:38:57 -0500

    chore: cosmetic changes

--- public/js/app.js
+++ public/js/app.js
@@ -17,8 +17,8 @@ console.log("Client Side JS file is loaded");
 
 const weatherForm = document.querySelector("form");
 const searchData = document.querySelector("input");
-const messageOne = document.querySelector('#para1');
-const messageTwo = document.querySelector('#para2');
+const messageOne = document.querySelector("#para1");
+const messageTwo = document.querySelector("#para2");
 
 // window.onload = function(){
 //     document.getElementById("search-button").style.display;
@@ -28,21 +28,21 @@ const messageTwo = document.querySelector('#para2');
 //     document.getElementById("loading-button").style.display = 'none';
 // }
 
-
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = searchData.value;
-  fetch(`http://localhost:3000/weather_tt?address=${location}`).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        messageOne.textContent = data.error;
-        messageTwo.textContent = '';
-      } else {
-
-        messageOne.textContent = data.address;
-        messageTwo.textContent = data.forecast;
-      }
-    });
-  });
+  fetch(`http://localhost:3000/weather_tt?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+          messageTwo.textContent = "";
+        } else {
+          messageOne.textContent = data.address;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
 });
